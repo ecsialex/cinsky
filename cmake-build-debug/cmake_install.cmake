2c4e1bd6cd4e289a957a3f79a69941b2a97e7251
Alexander Écsi <ecsialex@fit.cvut.cz>
2022-11-09 14:12:29 +0100

    cmake min req changed

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/sandor/CLionProjects/cinsky
+# Install script for directory: /home/sandor/Documents/GreenFox/cinsky
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/sandor/CLionProjects/cinsky/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/sandor/Documents/GreenFox/cinsky/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
